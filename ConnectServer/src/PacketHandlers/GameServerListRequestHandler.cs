675af034dd60a0723ada65ab158f3f4a7b3c58f5
Elliott Linder <elliott.darfink@gmail.com>
2019-05-07 17:23:40 +0200

    feat(CS): resend server list to impaired clients

--- ConnectServer/src/PacketHandlers/GameServerListRequestHandler.cs
+++ ConnectServer/src/PacketHandlers/GameServerListRequestHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Muwesome.Network;
@@ -9,8 +10,10 @@ using Muwesome.Protocol.Connect.V20050502;
 namespace Muwesome.ConnectServer.PacketHandlers {
   internal class GameServerListRequestHandler : IPacketHandler<Client> {
     private readonly IGameServerController gameServerController;
+    private readonly ConcurrentBag<Client> impairedClients = new ConcurrentBag<Client>();
     private byte[] gameServerListPacket = Array.Empty<byte>();
     private int gameServerListPacketSize;
+    private int gameServersInResponse;
 
     /// <summary>Initializes a new instance of the <see cref="GameServerListRequestHandler"/> class.</summary>
     public GameServerListRequestHandler(IGameServerController gameServerController) {
@@ -30,6 +33,10 @@ namespace Muwesome.ConnectServer.PacketHandlers {
         this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
       }
 
+      if (this.gameServersInResponse == 0) {
+        this.impairedClients.Add(client);
+      }
+
       return true;
     }
 
@@ -40,6 +47,7 @@ namespace Muwesome.ConnectServer.PacketHandlers {
         this.gameServerListPacket = new byte[this.gameServerListPacketSize];
       }
 
+      this.gameServersInResponse = servers.Count;
       PacketHelper.CreatePacket<GameServerList, GameServerList.GameServer>(
         servers.Count,
         this.gameServerListPacket.AsSpan(),
@@ -51,6 +59,40 @@ namespace Muwesome.ConnectServer.PacketHandlers {
       }
     }
 
-    private void OnGameServerChange() => this.gameServerListPacketSize = 0;
+    private void OnGameServerChange() {
+      this.gameServerListPacketSize = 0;
+      this.FreeImpairedClients();
+    }
+
+    /// <summary>Frees any impaired game clients.</summary>
+    /// <remarks>
+    /// If the client receives an empty server list response, it becomes stuck
+    /// due to the user not being able to issue new requests, forcing them to
+    /// restart the client. To solve this, a new server list response is sent to
+    /// all clients that have previously received any empty response.
+    /// </remarks>
+    private void FreeImpairedClients() {
+      if (this.impairedClients.IsEmpty) {
+        return;
+      }
+
+      if (this.gameServerListPacketSize == 0) {
+        this.CreateServerListPacket(this.gameServerController.Servers);
+      }
+
+      if (this.gameServersInResponse == 0) {
+        return;
+      }
+
+      while (this.impairedClients.TryTake(out Client client)) {
+        if (!client.Connection.IsConnected) {
+          continue;
+        }
+
+        using (var writer = client.Connection.StartWrite(this.gameServerListPacketSize)) {
+          this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
+        }
+      }
+    }
   }
 }
