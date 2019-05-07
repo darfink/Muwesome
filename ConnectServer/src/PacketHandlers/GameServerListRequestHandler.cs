2993355028b1523fa2f7cb6eb1087c6627cd6851
Elliott Linder <elliott.darfink@gmail.com>
2019-05-07 18:02:29 +0200

    feat(CS): RWL lock for server list

--- ConnectServer/src/PacketHandlers/GameServerListRequestHandler.cs
+++ ConnectServer/src/PacketHandlers/GameServerListRequestHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Muwesome.Network;
 using Muwesome.Packet.Utility;
 using Muwesome.Protocol;
@@ -9,8 +10,9 @@ using Muwesome.Protocol.Connect.V20050502;
 
 namespace Muwesome.ConnectServer.PacketHandlers {
   internal class GameServerListRequestHandler : IPacketHandler<Client> {
+    private readonly ReaderWriterLockSlim packetLock = new ReaderWriterLockSlim();
     private readonly IGameServerController gameServerController;
-    private readonly ConcurrentBag<Client> impairedClients = new ConcurrentBag<Client>();
+    private readonly ISet<Client> impairedClients = new HashSet<Client>();
     private byte[] gameServerListPacket = Array.Empty<byte>();
     private int gameServerListPacketSize;
     private int gameServersInResponse;
@@ -25,40 +27,57 @@ namespace Muwesome.ConnectServer.PacketHandlers {
 
     /// <inheritdoc />
     public bool HandlePacket(Client client, Span<byte> packet) {
-      if (this.gameServerListPacketSize == 0) {
-        this.CreateServerListPacket(this.gameServerController.Servers);
-      }
+      this.packetLock.EnterUpgradeableReadLock();
+      try {
+        if (this.gameServerListPacketSize == 0) {
+          this.CreateServerListPacket(this.gameServerController.Servers);
+        }
 
-      using (var writer = client.Connection.StartWrite(this.gameServerListPacketSize)) {
-        this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
-      }
+        using (var writer = client.Connection.StartWrite(this.gameServerListPacketSize)) {
+          this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
+        }
 
-      if (this.gameServersInResponse == 0) {
-        this.impairedClients.Add(client);
+        if (this.gameServersInResponse == 0) {
+          this.packetLock.EnterWriteLock();
+          try {
+            this.impairedClients.Add(client);
+          } finally {
+            this.packetLock.ExitWriteLock();
+          }
+        }
+      } finally {
+        this.packetLock.ExitUpgradeableReadLock();
       }
 
       return true;
     }
 
     private void CreateServerListPacket(IReadOnlyCollection<GameServerEntry> servers) {
-      this.gameServerListPacketSize = PacketHelper.GetPacketSize<GameServerList, GameServerList.GameServer>(servers.Count);
+      this.packetLock.EnterWriteLock();
+      try {
+        this.gameServerListPacketSize = PacketHelper.GetPacketSize<GameServerList, GameServerList.GameServer>(servers.Count);
+        this.gameServersInResponse = servers.Count;
 
-      if (this.gameServerListPacket.Length < this.gameServerListPacketSize) {
-        this.gameServerListPacket = new byte[this.gameServerListPacketSize];
-      }
+        if (this.gameServerListPacket.Length < this.gameServerListPacketSize) {
+          this.gameServerListPacket = new byte[this.gameServerListPacketSize];
+        }
 
-      this.gameServersInResponse = servers.Count;
-      PacketHelper.CreatePacket<GameServerList, GameServerList.GameServer>(
-        servers.Count,
-        this.gameServerListPacket.AsSpan(),
-        out Span<GameServerList.GameServer> serverEntries);
+        PacketHelper.CreatePacket<GameServerList, GameServerList.GameServer>(
+          servers.Count,
+          this.gameServerListPacket.AsSpan(),
+          out Span<GameServerList.GameServer> serverEntries);
 
-      foreach (var (index, server) in servers.Select((s, i) => (i, s))) {
-        serverEntries[index].Code = server.Code;
-        serverEntries[index].Load = server.Load;
+        foreach (var (index, server) in servers.Select((s, i) => (i, s))) {
+          serverEntries[index].Code = server.Code;
+          serverEntries[index].Load = server.Load;
+        }
+      } finally {
+        this.packetLock.ExitWriteLock();
       }
     }
 
+    /// <summary>Called whenever any game server status has changed.</summary>
+    /// <remarks>Invalidates the game server packet cache.</remarks>
     private void OnGameServerChange() {
       this.gameServerListPacketSize = 0;
       this.FreeImpairedClients();
@@ -72,26 +91,33 @@ namespace Muwesome.ConnectServer.PacketHandlers {
     /// all clients that have previously received any empty response.
     /// </remarks>
     private void FreeImpairedClients() {
-      if (this.impairedClients.IsEmpty) {
-        return;
-      }
-
-      if (this.gameServerListPacketSize == 0) {
-        this.CreateServerListPacket(this.gameServerController.Servers);
-      }
-
-      if (this.gameServersInResponse == 0) {
-        return;
-      }
-
-      while (this.impairedClients.TryTake(out Client client)) {
-        if (!client.Connection.IsConnected) {
-          continue;
+      this.packetLock.EnterUpgradeableReadLock();
+      try {
+        if (this.impairedClients.Count == 0) {
+          return;
         }
 
-        using (var writer = client.Connection.StartWrite(this.gameServerListPacketSize)) {
-          this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
+        if (this.gameServerListPacketSize == 0) {
+          this.CreateServerListPacket(this.gameServerController.Servers);
         }
+
+        if (this.gameServersInResponse == 0) {
+          return;
+        }
+
+        foreach (Client client in this.impairedClients) {
+          if (!client.Connection.IsConnected) {
+            continue;
+          }
+
+          using (var writer = client.Connection.StartWrite(this.gameServerListPacketSize)) {
+            this.gameServerListPacket.AsSpan().Slice(0, this.gameServerListPacketSize).CopyTo(writer.Span);
+          }
+        }
+
+        this.impairedClients.Clear();
+      } finally {
+        this.packetLock.ExitUpgradeableReadLock();
       }
     }
   }
