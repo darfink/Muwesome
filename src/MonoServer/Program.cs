eb820bb4340024e21feca90b7b57ad875ba724f6
Elliott Linder <elliott.darfink@gmail.com>
2019-05-17 11:59:40 +0200

    feat: add monolithic server program
    This is program is only intended to be used during testing. Under
    production usage each server should instead be used as a separate
    micro-service communicating with RPCs.

---
+++ src/MonoServer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using Muwesome.Common;
+using Muwesome.Persistence;
+using Muwesome.Persistence.Initialization;
+using Muwesome.Persistence.NHibernate;
+
+namespace Muwesome.MonoServer {
+  /// <summary>Login server program.</summary>
+  internal static class Program {
+    /// <summary>The server entry point.</summary>
+    public static void Main() {
+      var repository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+      log4net.Config.BasicConfigurator.Configure(repository);
+
+      using (var persistenceContextProvider = new PersistenceContextProvider(PersistenceConfiguration.InMemory())) {
+        var initializer = new PersistenceInitializer(persistenceContextProvider);
+        initializer.CreateConfiguration();
+        initializer.CreateTestData();
+
+        using (var connectServer = CreateConnectServer())
+        using (var loginServer = CreateLoginServer(persistenceContextProvider))
+        using (var gameServer = CreateGameServer(connectServer)) {
+          connectServer.Start();
+          loginServer.Start();
+          gameServer.Start();
+          Task.WaitAny(gameServer.ShutdownTask, connectServer.ShutdownTask, InterruptSignal());
+          gameServer.Stop();
+          loginServer.Stop();
+          connectServer.Stop();
+        }
+      }
+    }
+
+    private static LoginServer.LoginServer CreateLoginServer(IPersistenceContextProvider persistenceContextProvider) {
+      var config = new LoginServer.Configuration();
+      return LoginServer.LoginServerFactory.Create(config, persistenceContextProvider);
+    }
+
+    private static ConnectServer.ConnectServer CreateConnectServer() {
+      var config = new ConnectServer.Configuration();
+      return ConnectServer.ConnectServerFactory.Create(config);
+    }
+
+    private static GameServer.GameServer CreateGameServer(IGameServerRegistrar gameServerRegistrar) {
+      var config = new GameServer.Configuration();
+      return GameServer.GameServerFactory.Create(config, gameServerRegistrar);
+    }
+
+    private static Task InterruptSignal() {
+      var cancellation = new TaskCompletionSource<bool>();
+      Console.CancelKeyPress += (sender, eventArgs) => {
+        eventArgs.Cancel = true;
+        cancellation.SetResult(true);
+      };
+      return cancellation.Task;
+    }
+  }
+}
